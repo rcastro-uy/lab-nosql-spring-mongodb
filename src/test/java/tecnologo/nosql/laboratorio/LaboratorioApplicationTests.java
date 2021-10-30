44103fadfc146b257e7a74d3d3aa9b35d2b23fe4
Rodrigo Castro <tfimally@gmail.com>
2021-10-30 20:53:28 -0300

    Commit Inicial

---
+++ src/test/java/tecnologo/nosql/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package tecnologo.nosql.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
